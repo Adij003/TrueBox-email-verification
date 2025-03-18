2964149b1b96c16df8252d2f041d708af4106417
adi jain <jainadi1717@gmail.com>
2025-03-18 16:15:24 +0530

    single verify bug fixed

--- frontend/src/pages/app/dashboard.jsx
+++ frontend/src/pages/app/dashboard.jsx
@@ -86,8 +86,7 @@ export default function Page() {
   if (emailRegex.test(email)) {
     try {
       const resultAction = await dispatch(verifySingleEmail({ email })).unwrap();
-      console.log('already existing email: ', resultAction.data.data.message)
-      toast.success(`Verification Done: ${resultAction?.data?.newVerification?.message || `${resultAction?.data?.data?.message}`}`, {
+      toast.success(`Verification Done: ${resultAction?.data?.newVerification?.message || `${resultAction?.data?.data?.message}` || 'Email is valid'}`, {
         duration: Infinity,
         style: { marginTop: '15px' },
       });
